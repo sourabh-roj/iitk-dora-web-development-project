54995870e0f3299f505b4039fd89dd83aa68836f
sourabh-roj <sourabhroj@gmail.com>
2026-05-27 08:20:23 +0530

    FIX

--- publications/blogs/blogs.php
+++ publications/blogs/blogs.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IITK Blog</title>
-    <link rel="stylesheet" href="/DORA/Internship/assets/CSS/blog.css">
+    <link rel="stylesheet" href="../../assets/CSS/blog.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
